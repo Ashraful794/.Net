eabaf6cde7f62122fa861a3c7f154c3d0b86ed42
AshrafulIslambnxnetworks <ai568@bnxnetworks.com>
2022-05-22 20:01:45 +0600

    Admin

---
+++ RoleBased/Controllers/RegisterController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using RoleBased.Models;
+
+namespace RoleBased.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+
+        Ash_DBEntities db = new Ash_DBEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(tbl_user model)
+        {
+            db.tbl_user.Add(model);
+
+            db.SaveChanges();
+
+            return RedirectToAction("Login");
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(MemberShip model)
+        {
+            tbl_user user = (from st in db.tbl_user where st.Username == model.Username && st.Password == model.Password select st).FirstOrDefault();
+            
+            if(user!=null)
+            {
+                FormsAuthentication.SetAuthCookie(user.Username,false);
+                return RedirectToAction("Index","tbl_Student");
+            }
+
+            ViewBag.Error = "InCorrect User Name or Password";
+            return View();
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ RoleBased/WebRoleProvider.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using RoleBased.Models;
+namespace RoleBased
+{
+    public class WebRoleProvider : RoleProvider
+    {
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            Ash_DBEntities db = new Ash_DBEntities();
+            var result = (from user in db.tbl_user join role in db.tbl_Role on user.Id equals role.UserId where user.Username== username select
+                 role.Role).ToArray();
+            return result;
+
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
